b0eab02a721f1100c4a9dab8ce75adbbe40d4167
peanuts <849421294godw@gmail.com>
2021-05-20 00:07:37 +0800

    complete session base

--- src/app/app.go
+++ src/app/app.go
@@ -4,9 +4,7 @@ import (
 	"framework/cfgargs"
 	"framework/db"
 	"framework/logger"
-	"framework/net/socketio"
-	"gate/handler"
-	sio "github.com/googollee/go-socket.io"
+	"gate/server"
 	"sync"
 )
 
@@ -16,8 +14,8 @@ var (
 )
 
 type App struct {
-	sioSrv *socketio.Server
-	cfg    *cfgargs.SrvConfig
+	srv *server.Server
+	cfg *cfgargs.SrvConfig
 }
 
 func GetApp() *App {
@@ -36,33 +34,6 @@ func (a *App) Init(cfg *cfgargs.SrvConfig) {
 		logger.Fatal("init mongo db err: %v", err)
 		return
 	}
-	a.InitSocketIO(cfg)
-
-}
-
-func (a *App) InitSocketIO(cfg *cfgargs.SrvConfig) {
-	// sio srv init
-	a.sioSrv = socketio.NewServer()
-
-	a.sioSrv.OnConnect(func(conn sio.Conn) error {
-		logger.Info("socket.io connected, socket id :%v", conn.ID())
-		return nil
-	})
-
-	a.sioSrv.OnDisconnect(func(conn sio.Conn, s string) {
-		logger.Info("socket.io disconnected, socket id :%v", conn.ID())
-	})
-
-	a.sioSrv.OnError(func(conn sio.Conn, err error) {
-		logger.Error("socket.io on err: %v, id: %v", err, conn.ID())
-	})
-
-	// mount handlers
-	handlers := socketio.NewSIOHandlers()
-	handlers["hello"] = handler.Hello
-	handlers["chat"] = handler.Chat
-	a.sioSrv.MountHandlers("/", handlers)
-
-	// run
-	go a.sioSrv.Run(cfg) //nolint: errcheck
+	a.srv = server.NewServer()
+	a.srv.InitSocketIO(cfg)
 }
